9040ce8855514251b3ed069fb518f6d892920b0b
Bogdan Balasoiu <balasoiu.bogdan@gmail.com>
2026-03-11 10:14:24 +0200

    Feature/rating review crud (#9)
    * [FEAT] Added CRUD for Review & Rating
    
    * [FEAT] Added user validation for Rating and Review services

--- src/main/java/ro/unibuc/prodeng/controller/ReviewController.java
+++ src/main/java/ro/unibuc/prodeng/controller/ReviewController.java
@@ -2,20 +2,19 @@ package ro.unibuc.prodeng.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
+import ro.unibuc.prodeng.exception.EntityNotFoundException;
 import ro.unibuc.prodeng.request.CreateReviewRequest;
+import ro.unibuc.prodeng.request.UpdateReviewRequest;
 import ro.unibuc.prodeng.response.ReviewResponse;
 import ro.unibuc.prodeng.service.ReviewService;
 
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -25,17 +24,26 @@ public class ReviewController {
     }
 
     @PostMapping
-    public ReviewResponse addReview(@RequestBody CreateReviewRequest request) {
-        return reviewService.addReview(request);
+    public ResponseEntity<ReviewResponse> addReview(@Valid @RequestBody CreateReviewRequest request) {
+        ReviewResponse response = reviewService.addReview(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteReview(@PathVariable String id) {
-        reviewService.deleteReview(id);
+    public ResponseEntity<Void> deleteReview(@PathVariable String id, @RequestParam String userId) throws EntityNotFoundException {
+        reviewService.deleteReview(id, userId);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/movie/{movieId}")
-    public List<ReviewResponse> getReviews(@PathVariable String movieId) {
-        return reviewService.getReviewsForMovie(movieId);
+    public ResponseEntity<List<ReviewResponse>> getReviews(@PathVariable String movieId) {
+        List<ReviewResponse> reviews = reviewService.getReviewsForMovie(movieId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewResponse> updateReview(@PathVariable String id, @Valid @RequestBody UpdateReviewRequest request) throws EntityNotFoundException {
+        ReviewResponse response = reviewService.updateReview(id, request);
+        return ResponseEntity.ok(response);
     }
 }
