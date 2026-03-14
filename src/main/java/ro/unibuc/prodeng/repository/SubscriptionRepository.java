d4f05b204b8c45172cb9d93f6d011aff625dbd27
Pacobaba <93642408+Pacobaba@users.noreply.github.com>
2026-03-14 17:20:40 +0200

    Revert "Features/movies subscriptions genres"

--- src/main/java/ro/unibuc/prodeng/repository/SubscriptionRepository.java
+++ src/main/java/ro/unibuc/prodeng/repository/SubscriptionRepository.java
@@ -1,7 +1,6 @@
 package ro.unibuc.prodeng.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import ro.unibuc.prodeng.model.SubscriptionEntity;
 
 @Repository
 public interface SubscriptionRepository extends MongoRepository<SubscriptionEntity, String> {
-    Optional<SubscriptionEntity> findByUserId(String userId);
+
 }
--- src/main/java/ro/unibuc/prodeng/request/CreateSubscriptionRequest.java
+++ src/main/java/ro/unibuc/prodeng/request/CreateSubscriptionRequest.java
@@ -1,6 +1,4 @@
 package ro.unibuc.prodeng.request;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateSubscriptionRequest(
@@ -8,9 +6,7 @@ public record CreateSubscriptionRequest(
     String  userId,
     @NotBlank(message = "Name is required")
     String  name,
-    @Min(value = 0, message = "Must be a passable price!")
     Float   price,
-    @Min(value = 0, message = "Must be a passable duration!")
     Integer duration,
     String end_date
 ) {}
