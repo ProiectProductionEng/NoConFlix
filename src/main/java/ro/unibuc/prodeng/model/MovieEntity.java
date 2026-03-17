a1f0726a05b4b3926d0e9eb3411229f0c9039cf0
Bogdan Balasoiu <balasoiu.bogdan@gmail.com>
2026-03-17 16:18:24 +0200

    [FEAT] Added smart search for movies (#18)

--- src/main/java/ro/unibuc/prodeng/model/MovieEntity.java
+++ src/main/java/ro/unibuc/prodeng/model/MovieEntity.java
@@ -1,13 +1,19 @@
 package ro.unibuc.prodeng.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "movies")
 public record MovieEntity(
     @Id String id,
+
+    @TextIndexed
     String  title,
+
+    @TextIndexed
     String  description,
+    
     String  genreId,
     String  releaseDate,
     Integer duration,
--- src/main/java/ro/unibuc/prodeng/repository/MovieRepository.java
+++ src/main/java/ro/unibuc/prodeng/repository/MovieRepository.java
@@ -3,6 +3,7 @@ package ro.unibuc.prodeng.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.unibuc.prodeng.model.MovieEntity;
 
@@ -12,4 +13,9 @@ public interface MovieRepository extends MongoRepository<MovieEntity, String> {
     //List<MovieEntity> findByMovieId(String movieId);
 
     List<MovieEntity> findAllByOrderByTotalViewsDesc();
+
+    List<MovieEntity> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ $text: { $search: ?0 } }")
+    List<MovieEntity> searchByText(String text);
 }
