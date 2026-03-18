48baf47d6bb4161c204af7186cb1f11843458e62
Pacobaba <93642408+Pacobaba@users.noreply.github.com>
2026-03-18 08:17:15 +0000

    [FEAT] Recommended films

--- src/main/java/ro/unibuc/prodeng/service/MovieService.java
+++ src/main/java/ro/unibuc/prodeng/service/MovieService.java
@@ -1,6 +1,11 @@
 package ro.unibuc.prodeng.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+<<<<<<< HEAD
+import java.util.LinkedHashSet;
+=======
+>>>>>>> 0071533 ([FEAT] Recommended films)
 import java.util.List;
 import java.util.Set;
 import java.time.*;
